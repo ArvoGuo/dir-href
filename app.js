778c2005684abf174761b55213f3ad6fda920523
Arvo <ArvoGuo@gmail.com>
2015-04-17 11:57:48 +0800

    init

---
+++ app.js
@@ -0,0 +1,67 @@
+var config   = require('./config');
+var path     = require('path');
+var fs       = require('fs');
+var dir      = path.dirname(__dirname);
+var host     = config.host;
+var template = config.temp;
+var html     = '';
+
+
+/**===================================**/
+/**============ prototype ============**/
+/**===================================**/
+Array.prototype.remove = function () {
+  var what, a = arguments, L = a.length, ax;
+  while (L && this.length) {
+    what = a[--L];
+    while ((ax = this.indexOf(what)) !== -1) {
+      this.splice(ax, 1);
+    }
+  }
+  return this;
+};
+
+/**===================================**/
+/**============= Function ============**/
+/**===================================**/
+
+function ignoreFiles(files) {
+  files.remove('.DS_Store').remove('.git').remove('node_modules').remove('bower_compontents').remove('lib');
+}
+
+function getFiles(path, files_) {
+  files_    = files_ || [];
+  var files = fs.readdirSync(path);
+  ignoreFiles(files);
+  if (files.indexOf('index.html') > 0) {
+    files_.push((path + '/index.html').replace(dir,host));
+    return files_;
+  }
+  for (var i in files) {
+    if (!files.hasOwnProperty(i)) {
+      continue;
+    }
+    var _path = path + '/' + files[i];
+    if (fs.statSync(_path).isDirectory()) {
+      getFiles(_path,files_);
+    }
+  }
+  return files_;
+}
+
+/**===================================**/
+/**=============== Main ==============**/
+/**===================================**/
+html = (function() {
+  var h = '';
+  getFiles(dir).forEach(function(item){
+    h += template.replace(/{data}/g, item);
+  });
+  return h;
+})();
+
+fs.writeFileSync('catalogue.html', html);
+
+
+
+
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  host: 'http://c.oooooo6.com',
+  temp: '<a href="{data}">{data}</a>\n'
+};
