81303ec82af7b54acfad8c7ebbb57967fa773d8b
Arvo <ArvoGuo@gmail.com>
2015-04-23 11:09:08 +0800

    update

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
-var config        = require('./temp/config.js');
-var templatePath  = __dirname + '/temp/template.html';
-var path          = require('path');
-var readlineSync  = require('readline-sync');
-var fs            = require('fs');
-var dir           = process.argv[2] || process.cwd();
-var host          = config.host;
-var template      = config.temp;
-var html          = '';
+var config             = require('./temp/config.js');
+var templatePath       = __dirname + '/temp/template.html';
+var path               = require('path');
+var readlineSync       = require('readline-sync');
+var fs                 = require('fs');
+var dir                = process.argv[2] || process.cwd();
+var host               = config.host;
+var template           = config.temp;
+var html               = '';
 var needIgnoreFiles    = [
     '.DS_Store',
     '.git',
     'node_modules',
     'bower_compontents',
-    'lib'
+    'lib',
+    'test'
 ];
-var matchTag      = '.html';
+var distFileName       = 'catalogue.html';
+var matchTag           = '.html';
 
 /**===================================**/
 /**============ prototype ============**/
@@ -54,7 +56,7 @@ function getFiles(path, files_) {
     if (fs.statSync(_path).isDirectory()) {
       getFiles(_path,files_);
     } else {
-      if (files[i].match(matchTag)) {
+      if (files[i].match(matchTag) && files[i] != distFileName) {
         files_.push((path + '/' + files[i]).replace(dir,host));
       }
     }
@@ -69,15 +71,17 @@ function getFiles(path, files_) {
 html = (function() {
   var h = '';
   getFiles(dir).forEach(function(item){
-    var urlName = readlineSync.question('href ( ' + item + ' ) name is : ');
-    h += template.replace(/{url}/g, item).replace(/{urlName}/g,urlName);
+    var urlName = readlineSync.question('href ( ' + item + ' ) name is : ').toString();
+    if (urlName) {
+      h += template.replace(/{url}/g, item).replace(/{urlName}/g,urlName);
+    }
   });
   return h;
 })();
 
 html = fs.readFileSync(templatePath, {encoding: 'utf8'}).replace(/{{content}}/g, html);
 
-fs.writeFileSync('catalogue.html', html);
+fs.writeFileSync(distFileName, html);
 
 
 
